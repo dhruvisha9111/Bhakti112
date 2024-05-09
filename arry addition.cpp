45964b0212d7b0849d75b4484745e7c2f8e6dcc3
Dhruvisha <dhruvisha2008@gmail.com>
2024-05-09 14:12:37 +0530

    Add files via upload

---
+++ arry addition.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a1[3],b1[3],c1[3],i;
+    cout << "enter the value a1=" << endl;
+    cin >> a1[i];
+    cout << "enter the value b1=" << endl;
+    cin >> b1[i];
+    cout << "enter the value c1=" << endl;
+    cin >> c1[i];
+    cout < c1[i]=a1[i]+b1[i];
+    cin >> c1[i];
+    return 0;
+}
